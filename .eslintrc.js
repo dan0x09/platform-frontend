48eb7ae4b95ab8d01dfd5014fcde58f9efacca41
Malte Hansen <malte.hanseat@gmail.com>
2022-02-21 15:28:22 +0100

    Ignore floating Promises

--- .eslintrc.js
+++ .eslintrc.js
@@ -108,6 +108,7 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-empty-interface': 'error',
         '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
         '@typescript-eslint/no-inferrable-types': [
             'error',
             {
