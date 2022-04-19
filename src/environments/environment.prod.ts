ba72b5ecba9e6faf40e5e38718ee6831d1a93821
Malte Hansen <malte.hanseat@gmail.com>
2022-04-19 16:09:44 +0200

    Update URL of API for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://silage-control-platform.eu-central-1.elasticbeanstalk.com',
+    apiBaseUrl: 'http://api.silagecontrol.de',
 };
