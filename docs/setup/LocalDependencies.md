fea35ce9691bfa13e890484f6ae019eabc2fa443
h3nnut <lrhemmerling@gmail.com>
2022-07-19 15:47:38 +0200

    fixed a problem, where react has to be installed as peerDependency - updated docs

--- docs/setup/LocalDependencies.md
+++ docs/setup/LocalDependencies.md
@@ -4,3 +4,11 @@
 Lerna organizes packages under ./packages/ (configurable in lerna.json). These are nested node projects. To add a dependency to a local package, use a npm script (if you use a local lerna installation) and type:
 
 ``` npm i && cd ./packages/install-to/ && lerna add pkg-to-install ```
+
+## dependencies vs. peerDependencies
+An easily confusing problem is when to use peerDependencies. They can help with plugin like packages, which use f.e. react as dependency. If the main package, which imports the plugin, uses react too, some functions of react might fail. An example for such a function is `useState`. To overcome this problem, one should specify react as peerDependency in the plugin.
+
+More on peerDependencies can be found here: [Peer Dependencies | Node.js](https://nodejs.org/en/blog/npm/peer-dependencies/)
+
+## devDependencies
+These dependencies come with an useful advantage: They are only included while in development. It might be necessary to install a certain compiler to create a build, but this compiler should not be included in the build itself. To accomplish that, the devDependencies section of a package.json features all modules that should be handled this way. Lerna f.e. is such a devDependency.
--- packages/workspace/test/src/App.tsx
+++ packages/workspace/test/src/App.tsx
@@ -1,33 +1,14 @@
+import React, { useState } from 'react'
 import TEST2 from 'test2'
-import { Page, Site, PageLayout, SiteAlign, Button, FAB, Chart, ChartData, ChartDataType } from 'sgcomponents'
+import { Page, Site, PageLayout, SiteAlign, Button, FAB, Chart, ChartDataType, ChartDataPoint } from 'sgcomponents'
+import { fetchTemperatureData } from 'sgapi'
+
+
 import './Style.css'
 
-const testData: ChartData[] = [
-    {
-        yName: "why",
-        color: "red",
-        points: [
-            ["jo", 1],
-            [.5, 0],
-            [0, .5],
-            [1, 0],
-            ["lol", 1]
-        ]
-    },
-    {
-        yName: "hi",
-        type: ChartDataType.AREA,
-        points: [
-            ["jo", 2],
-            [.5, 1],
-            [0, 1.5],
-            [1, 2],
-            ["lol", .5]
-        ]
-    }
-]
+const App: React.FC = () => {
+    const [data, setData] = useState([] as ChartDataPoint[]);
 
-const App = () => {
     return (
         <PageLayout>
             <h2 className='color0'>{TEST2}</h2>
@@ -38,13 +19,18 @@ const App = () => {
 
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure accusantium, aut nesciunt vel deleniti officiis sit quo optio ducimus ab quae odit esse atque exercitationem accusamus laborum fugit distinctio magnam! Lorem ipsum dolor sit, amet consectetur adipisicing elit. In numquam eveniet perspiciatis quia, eos facilis alias, vero fuga ad, inventore aperiam libero veritatis explicabo quibusdam fugit dicta tempore! Recusandae, maiores?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente atque ducimus maxime illum vero, aliquid quae voluptate modi beatae dignissimos minus? Eaque sapiente aperiam aut quidem velit nisi hic temporibus?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi quidem adipisci asperiores facilis quaerat qui quibusdam animi dolor, ipsum provident aperiam eligendi esse optio soluta. Excepturi mollitia tenetur quis illo?Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ex facere amet, facilis vero fugit similique dolore! Quo, dolorum. Possimus, molestias veniam? Esse, maiores minima quae corrupti nisi modi ducimus.LoremLorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque placeat sint nobis fugiat aliquam quos blanditiis, totam ducimus corrupti dolore amet commodi, iure voluptatum libero quibusdam voluptatem, eos porro ipsam.</p>
                 
-                    <FAB onClick={() => {}}/>
+                    <FAB onClick={async() => setData(await fetchTemperatureData() as ChartDataPoint[])}/>
                 </Site>
 
                 <Site align={SiteAlign.TOP}>
                     <h2>Ok</h2>
 
-                    <Chart data={testData} />
+                    <Chart data={[{
+                        yName: "temperature",
+                        type: ChartDataType.AREA,
+                        color: "red",
+                        points: data
+                    }]} />
                 </Site>
 
             </Page>
