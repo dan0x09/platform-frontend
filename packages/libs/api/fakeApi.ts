a1e794a60e080874ba5bda04f5d5944d20b45a35
h3nnut <lrhemmerling@gmail.com>
2022-07-19 15:47:44 +0200

    better random data

--- packages/libs/api/fakeApi.ts
+++ packages/libs/api/fakeApi.ts
@@ -2,8 +2,8 @@
 export async function fetchTemperatureData() {
 	const data = []
 	for(let i = 0; i < 100; i++) {
-		const x = Math.random() * 24, //hours
-			y = Math.random() * 35	  //temp C
+		const x = Math.round(Math.random() * 24), //hours
+			y = Math.round(Math.random() * 35)	  //temp C
 		data.push([x, y])
 	}
 	
