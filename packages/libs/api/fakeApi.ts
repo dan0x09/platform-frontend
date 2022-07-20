d647393c7182f6ca5d1ca401e3e53ff1dbc3f88d
h3nnut <lrhemmerling@gmail.com>
2022-07-20 11:36:39 +0200

    removed "h" in fakeApi to test displayX of charts

--- packages/libs/api/fakeApi.ts
+++ packages/libs/api/fakeApi.ts
@@ -2,7 +2,7 @@
 export async function fetchTemperatureData() {
 	const data = []
 	for(let i = 0; i < 24; i++) {
-		const x = i + 1 + "h", // hours
+		const x = i + 1, // hours
 			y = Math.random() * 35 // temp C
 		data.push([x, y])
 	}
