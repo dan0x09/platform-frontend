f07e3fb9cb824083643490065b1a4df7c4eaa34d
h3nnut <lrhemmerling@gmail.com>
2022-07-29 13:22:46 +0200

    some changes to UI

--- packages/libs/components/src/components/ErrorPage.tsx
+++ packages/libs/components/src/components/ErrorPage.tsx
@@ -1,24 +1,35 @@
 import React from "react"
+import { PageLayout } from "./Layouts"
 
-import { Page, Site } from './Page'
+import { Page, Row, Site } from './Page'
 
 import "./style.css"
 
 export const ErrorPage: React.FC = () => {
 	return (
-		<Page>
-			<Site>
-				<h1>404</h1>
+		<PageLayout>
+			<Row spaceAround space="20%">
+				<h1>ERROR 404</h1>
+			</Row>
 
-				<h2>Sorry!</h2>
-				<p>but this page was not found</p>
-			</Site>
+			<Page>
+				<Site>
+					<div>
+						<h1>Sorry!</h1>
+						<p>but this page was not found...</p>
 
-			<Site>
-				<h2>Here is what you can do:</h2>
+						<h2>Here is what you can do:</h2>
 
-				<p>{"Check, whether this is really the site you are trying to connect to: " + window.location}</p>
-			</Site>
-		</Page>
+						<p>{"Check, whether this is really the site you are trying to connect to: " + window.location}</p>
+
+						<p>If this error persists, contact our support team under _</p>
+					</div>
+				</Site>
+
+				<div/>
+			</Page>
+
+			<div/>
+		</PageLayout>
 	)
 }
