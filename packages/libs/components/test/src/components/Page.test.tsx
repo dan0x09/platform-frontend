710a9ff87446bc22b0a387ceb57aa276c99d3e47
h3nnut <lrhemmerling@gmail.com>
2022-08-03 11:56:09 +0200

    Added tests for Page related components

--- packages/libs/components/test/src/components/Page.test.tsx
+++ packages/libs/components/test/src/components/Page.test.tsx
@@ -3,8 +3,52 @@
  */
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import { Page } from "../../../src"
- 
+import { Page, Row, Site } from "../../../src"
+
+describe('Row', function () {
+    it('should render all components', function () {
+        render(
+            <Row>
+                <h1
+                    data-testid="row-h1">
+                        TEST
+                </h1>
+                <h2
+                    data-testid="row-h2">jo</h2>
+
+                <p
+                    data-testid="row-p">some testing. is everything at its place?</p>
+            </Row>
+        )
+
+        expect(screen.getByTestId("row-h1").textContent).toBe("TEST")
+        expect(screen.getByTestId("row-h2").textContent).toBe("jo")
+        expect(screen.getByTestId("row-p").textContent).toBe("some testing. is everything at its place?")
+    })
+})
+
+describe('Site', function () {
+    it('should render all components', function () {
+        render(
+            <Site>
+                <h1
+                    data-testid="site-h1">
+                        TEST
+                </h1>
+                <h2
+                    data-testid="site-h2">jo</h2>
+
+                <p
+                    data-testid="site-p">some testing. is everything at its place?</p>
+            </Site>
+        )
+
+        expect(screen.getByTestId("site-h1").textContent).toBe("TEST")
+        expect(screen.getByTestId("site-h2").textContent).toBe("jo")
+        expect(screen.getByTestId("site-p").textContent).toBe("some testing. is everything at its place?")
+    })
+})
+
 describe('Page', function () {
     it('should render all components', function () {
         render(
