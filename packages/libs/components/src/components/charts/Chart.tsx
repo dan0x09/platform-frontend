7f73de2b917b7cb43411e032f50422da5e5c3c4e
h3nnut <lrhemmerling@gmail.com>
2022-08-03 10:44:18 +0200

    Fixed Charts to always show

--- packages/libs/components/src/components/charts/Chart.tsx
+++ packages/libs/components/src/components/charts/Chart.tsx
@@ -4,8 +4,8 @@ import {
     XAxis, YAxis, CartesianGrid, Legend, Tooltip,
     Line, Area
 } from "recharts"
-import { ChartData, ChartDataType, ChartProps } from '../prop-types'
 import CSS from 'csstype'
+import { ChartData, ChartDataType, ChartProps } from '../prop-types'
 import "../style.css"
 
 const dataKeyX = "name"
@@ -18,35 +18,37 @@ function mapFormatter(value: number, name: string, data: ChartData[]) {
 
 const Chart: React.FC<ChartProps> = ({style={} as CSS.Properties, data, displayX=v=>v}) => {
     return (
-        <ResponsiveContainer>
-            <ComposedChart style={style}>
-                <XAxis dataKey={dataKeyX} type="category" allowDuplicatedCategory={false} />
-                <YAxis />
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Legend verticalAlign="top" height={36} />
-                <Tooltip formatter={(value: number, name: string) => mapFormatter(value, name, data)} />
-                {   // Chart data goes here
-                    data.map(({yName, style={color: "#8884d8", backgroundColor: "#8884d8"}, type, points}) => {
-                        const dataSet = points.map(([x, y]) => ({
-                            [dataKeyX]: "" + displayX(x),
-                            [yName]: y
-                        }))
-                        const {color = "#8884d8", backgroundColor = "#8884d8"} = style
-                        switch(type) {
-                            case ChartDataType.AREA:
-                                return <Area key={"" + yName} dataKey={yName} name={yName} 
-                                    // @ts-ignore
-                                    data={dataSet}
-                                    type="monotone" fill={backgroundColor} stroke={color} />
-                            // case ChartDataType.BAR:
-                            //     return <Bar key={"" + yName} dataKey={yName} name={yName} fill={backgroundColor} stroke={color} />
-                            default: //LINE
-                                return <Line key={"" + yName} dataKey={yName} name={yName} data={dataSet} type="monotone" stroke={color} />
-                        }
-                    })
-                }
-            </ComposedChart>
-        </ResponsiveContainer>
+        <div style={{width: '100%'}}>
+            <ResponsiveContainer aspect={1.3}>
+                <ComposedChart style={style}>
+                    <XAxis dataKey={dataKeyX} type="category" allowDuplicatedCategory={false} />
+                    <YAxis />
+                    <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                    <Legend verticalAlign="top" height={36} />
+                    <Tooltip formatter={(value: number, name: string) => mapFormatter(value, name, data)} />
+                    {   // Chart data goes here
+                        data.map(({yName, style={color: "#8884d8", backgroundColor: "#8884d8"}, type, points}) => {
+                            const dataSet = points.map(([x, y]) => ({
+                                [dataKeyX]: "" + displayX(x),
+                                [yName]: y
+                            }))
+                            const {color = "#8884d8", backgroundColor = "#8884d8"} = style
+                            switch(type) {
+                                case ChartDataType.AREA:
+                                    return <Area key={"" + yName} dataKey={yName} name={yName} 
+                                        // @ts-ignore
+                                        data={dataSet}
+                                        type="monotone" fill={backgroundColor} stroke={color} />
+                                // case ChartDataType.BAR:
+                                //     return <Bar key={"" + yName} dataKey={yName} name={yName} fill={backgroundColor} stroke={color} />
+                                default: //LINE
+                                    return <Line key={"" + yName} dataKey={yName} name={yName} data={dataSet} type="monotone" stroke={color} />
+                            }
+                        })
+                    }
+                </ComposedChart>
+            </ResponsiveContainer>
+        </div>
     )
 }
 
