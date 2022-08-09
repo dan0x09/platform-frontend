851837ea91fa9370e8e7806a28b92c605f0d7e7d
h3nnut <lrhemmerling@gmail.com>
2022-08-09 13:07:51 +0200

    fixed some default styling to be more intuitive - Pages and Sites - GridLayout

--- packages/libs/components/src/components/Page.tsx
+++ packages/libs/components/src/components/Page.tsx
@@ -72,11 +72,11 @@ export const Page: React.FC<PageProps> = ({children, style={} as CSS.Properties,
         else {
             return (
                 <div className="_PageMobile color0 PageMobile">
-                    <div className={!mobileSwitch ? "__PageMobileWrapper" : "__PageMobileWrapper __PageMobileWrapperCollapsed"}>
+                    <div className={!mobileSwitch ? "__PageMobileClosingWrapper" : "__PageMobileClosingWrapper __PageMobileClosingWrapperCollapsed"}>
                         {children[0]}
                     </div>
     
-                    <div className={mobileSwitch ? "__PageMobileWrapper" : "__PageMobileWrapper __PageMobileWrapperCollapsed"}>
+                    <div className={mobileSwitch ? "__PageMobileClosingWrapper" : "__PageMobileClosingWrapper __PageMobileClosingWrapperCollapsed"}>
                         {children[1]}
                     </div>
     
