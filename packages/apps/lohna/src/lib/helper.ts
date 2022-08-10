643ccbe27116b34ecc5b458138299ea6fd3f81a9
h3nnut <lrhemmerling@gmail.com>
2022-08-10 13:02:28 +0200

    removed outdated files and functions - these are handled by sgwidgets now

--- packages/apps/lohna/src/lib/helper.ts
+++ packages/apps/lohna/src/lib/helper.ts
@@ -1,17 +1,3 @@
-import { DataState, DataStateWrapper } from "../types";
-
 export function isMobileThreshold(windowWidth: number) {
 	return windowWidth < 1200
-}
-
-export async function createDataStateWrapper(dataState: DataState) {
-	return {
-		dataState,
-		dataSets: dataState.getDataSets ? await dataState.getDataSets() : []
-	} as DataStateWrapper
-}
-
-export async function hydrateDataStateWrapper(dataStateWrapper: DataStateWrapper) {
-	dataStateWrapper.dataSets = dataStateWrapper.dataState.getDataSets ? await dataStateWrapper.dataState.getDataSets() : []
-	return dataStateWrapper
 }
