0b5cb3e48adb6b5dbb49f3bca2e5988302a9a4af
h3nnut <lrhemmerling@gmail.com>
2022-08-18 13:39:08 +0200

    Implemented new applicationwrapper

--- packages/apps/lohna/src/Router.tsx
+++ packages/apps/lohna/src/Router.tsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import { ApplicationWrapper, ErrorPage } from "sgcomponents"
+
 import Login from "./pages/Login"
 import Silos from "./pages/Silos"
 import Data from "./pages/Data"
-import { ErrorPage } from "sgcomponents"
 
 export default function Router() {
 	return (
 	  <BrowserRouter>
-		<Routes>
-		  <Route path="/">
-			<Route index element={<Login />} />
-			<Route path="silos" element={<Silos />} />
-			<Route path="silo" element={<Data />} />
-			<Route path="*" element={<ErrorPage />} />
-		  </Route>
-		</Routes>
+		<ApplicationWrapper>
+			<Routes>
+				<Route path="/">
+					<Route index element={<Login />} />
+					<Route path="silos" element={<Silos />} />
+					<Route path="silo" element={<Data />} />
+					<Route path="*" element={<ErrorPage />} />
+				</Route>
+			</Routes>
+		</ApplicationWrapper>
 	  </BrowserRouter>
 	)
   }
