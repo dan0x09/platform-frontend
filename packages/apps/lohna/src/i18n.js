724d61e418de2b95aac7084d623b02380d95c63d
h3nnut <lrhemmerling@gmail.com>
2022-08-19 11:09:20 +0200

    added locales with i18next

--- packages/apps/lohna/src/i18n.js
+++ packages/apps/lohna/src/i18n.js
@@ -22,7 +22,7 @@ i18n
   .init({
     fallbackLng: 'en',
     supportedLngs: ['de', 'en'],
-    debug: false,
+    debug: true,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
