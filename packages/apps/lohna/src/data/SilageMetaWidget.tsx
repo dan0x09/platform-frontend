cef0814fbbeb75f35a4e8878da7c143e11ec49b5
h3nnut <lrhemmerling@gmail.com>
2022-08-31 23:53:17 +0200

    Silage meta data - Custom widget - no need for display right now

---
+++ packages/apps/lohna/src/data/SilageMetaWidget.tsx
@@ -0,0 +1,43 @@
+import { Row, RowAlign } from "sgcomponents"
+import { WidgetDisplayType, WidgetState } from "sgwidgets"
+
+interface SilageMetaData {
+	id: number | string
+	name: string
+	date: string
+	description: string
+	customer: string
+}
+
+const MetaWidget = (silageId: string): WidgetState => {
+	return {
+		displayType: WidgetDisplayType.CUSTOM,
+		getData: async() => {
+			// async fetch of silage meta data
+			return {
+				id: silageId,
+				name: "Test silage",
+				date: "22.08.2022",
+				description: "A silage with grass.",
+				customer: "Customer " + (silageId + 1)
+			} as SilageMetaData
+		},
+		widgetComponent: ({data}) => {
+			const {id, name, date, description, customer} = data as SilageMetaData
+			return (
+				<div style={{width: '100%', height: '100%'}}>
+					<Row align={RowAlign.LEFT} space="10%">
+						<h2>{name}</h2>
+						<h3>:{id}/{date}</h3>
+					</Row>
+
+					<h3>{customer}</h3>
+
+					<p>{description}</p>
+				</div>
+			)
+		}
+	}
+}
+
+export default MetaWidget
