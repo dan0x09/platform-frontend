8dad65f08a188666cb2cbc3a6055a8dc9ffb75a5
dan0x09 <stu126132@mail.uni-kiel.de>
2023-02-09 14:02:27 +0100

    Add styling to Login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -16,21 +16,29 @@ export default function Login() {
   };
 
   return (
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>E-Mail-Adresse</p>
-          <input type="text" onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label>
-          <p>Passwort</p>
-          <input type="password" onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="login-wrapper h-screen flex justify-center items-center">
+      <div className="">
+        <h1 className="mb-3">Log In</h1>
+        <form className="flex flex-column items-center" onSubmit={handleSubmit}>
+          <label>
+            <p>E-Mail-Adresse</p>
+            <input className="mb-3 border-solid border-2" type="text" onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <label>
+            <p>Passwort</p>
+            <input
+              className="mb-3 border-solid border-2"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <div>
+            <button className="p-2 bg-sky-600 rounded-md text-slate-50" type="submit">
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
