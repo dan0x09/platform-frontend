025ca690e95287721c788e038e59cb0d601bf5a6
dan0x09 <stu126132@mail.uni-kiel.de>
2023-03-02 03:51:22 +0100

    Enhance design on login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -16,9 +16,10 @@ export default function Login() {
   };
 
   return (
-    <div className="login-wrapper h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center w-80 pt-5 pb-4 shadow rounded-lg bg-white">
-        <img src="svg/silage-control-logo.svg" className="w-36 mb-6 mt-4" />
+    <div className="login-wrapper h-screen flex flex-col justify-center items-center">
+      <img src="svg/silage-control-logo.svg" className="w-36 mb-6" />
+      <div className="flex flex-col items-center w-80 pt-5 pb-4 shadow-xl rounded-lg bg-white">
+        <h1 className="mb-4">Anmeldung</h1>
         <form className="flex flex-col items-center" onSubmit={handleSubmit}>
           <label className="mb-4">
             <p>E-Mail-Adresse</p>
@@ -28,7 +29,7 @@ export default function Login() {
             <p>Passwort</p>
             <input className="input input-bordered" type="password" onChange={(e) => setPassword(e.target.value)} />
           </label>
-          <button className="btn btn-primary mb-6" type="submit">
+          <button className="btn btn-primary mb-6 w-full" type="submit">
             Einloggen
           </button>
         </form>
