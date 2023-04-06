00583b7b96886954bcf46e5ffa8dcc7702231590
dan0x09 <stu126132@mail.uni-kiel.de>
2023-04-06 03:40:05 +0200

    Move Navbars into their own components

---
+++ src/components/NavbarContractor.tsx
@@ -0,0 +1,56 @@
+import { Button, Dropdown, Navbar, Menu } from 'react-daisyui';
+import { NavLink } from 'react-router-dom';
+import { useAuth } from '../authentication/AuthProvider';
+
+export default function NavbarContractor(args: any) {
+  const { onLogout } = useAuth();
+
+  return (
+    <div className="flex w-full items-center justify-center gap-2 shrink-0">
+      <Navbar>
+        <Navbar.Start>
+          <Dropdown>
+            <Button color="ghost" tabIndex={0} className="lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </Button>
+            <Dropdown.Menu tabIndex={0} className="w-52 menu-compact mt-3">
+              <li>
+                <NavLink to="/contractor/farms">Betriebe</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contractor/systems">Systeme</NavLink>
+              </li>
+              <li>
+                <NavLink to="/contractor/silage-heaps">Silagehaufen</NavLink>
+              </li>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Start>
+        <Navbar.Center className="hidden lg:flex">
+          <Menu horizontal className="p-0">
+            <Menu.Item>
+              <NavLink to="/contractor/farms">Betriebe</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+              <NavLink to="/contractor/systems">Systeme</NavLink>
+            </Menu.Item>
+            <Menu.Item>
+              <NavLink to="/contractor/silage-heaps">Silagehaufen</NavLink>
+            </Menu.Item>
+          </Menu>
+        </Navbar.Center>
+        <Navbar.End>
+          <Button onClick={() => onLogout()}>Ausloggen</Button>
+        </Navbar.End>
+      </Navbar>
+    </div>
+  );
+}
--- src/routes/Contractor.tsx
+++ src/routes/Contractor.tsx
@@ -1,62 +1,12 @@
 import { Fragment } from 'react';
-import { Button, Dropdown, Navbar, Menu } from 'react-daisyui';
-import { NavLink, Outlet } from 'react-router-dom';
-import { useAuth } from '../authentication/AuthProvider';
+import { Outlet } from 'react-router-dom';
+import NavbarContractor from '../components/NavbarContractor';
 
 export default function Contractor(args: any) {
-  const { onLogout } = useAuth();
-
   return (
     <Fragment>
-      <div className="flex w-full items-center justify-center gap-2 mb-6 shrink-0">
-        <Navbar {...args}>
-          <Navbar.Start>
-            <Dropdown>
-              <Button color="ghost" tabIndex={0} className="lg:hidden">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" />
-                </svg>
-              </Button>
-              <Dropdown.Menu tabIndex={0} className="w-52 menu-compact mt-3">
-                <li>
-                  <NavLink to="/contractor/farms">Betriebe</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contractor/systems">Systeme</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contractor/silage-heaps">Silagehaufen</NavLink>
-                </li>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Navbar.Start>
-          <Navbar.Center className="hidden lg:flex">
-            <Menu horizontal className="p-0">
-              <Menu.Item>
-                <NavLink to="/contractor/farms">Betriebe</NavLink>
-              </Menu.Item>
-              <Menu.Item>
-                <NavLink to="/contractor/systems">Systeme</NavLink>
-              </Menu.Item>
-              <Menu.Item>
-                <NavLink to="/contractor/silage-heaps">Silagehaufen</NavLink>
-              </Menu.Item>
-            </Menu>
-          </Navbar.Center>
-          <Navbar.End>
-            <Button {...args} onClick={() => onLogout()}>
-              Ausloggen
-            </Button>
-          </Navbar.End>
-        </Navbar>
-      </div>
-      <div className="farms-wrapper flex justify-center flex-1 shrink-1 overflow-y-auto">
+      <NavbarContractor />
+      <div className="farms-wrapper flex justify-center flex-1 shrink-1 overflow-y-auto mt-6">
         <div className="container">
           <Outlet></Outlet>
         </div>
