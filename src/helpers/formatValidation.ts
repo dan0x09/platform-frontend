7a673b4dc6361de1aa4836ee676cc2930fc2197b
dan0x09 <stu126132@mail.uni-kiel.de>
2023-04-06 05:25:52 +0200

    Add wip input validation for user invitation form

---
+++ src/helpers/formatValidation.ts
@@ -0,0 +1,7 @@
+export function isValidEmailFormat(mail: string) 
+{
+  const emailRegEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    
+    
+ return emailRegEx.test(mail);
+}
--- src/routes/Invite.tsx
+++ src/routes/Invite.tsx
@@ -1,50 +1,77 @@
-import { useState } from "react";
-import { useAuth } from "../authentication/AuthProvider";
+import { useState } from 'react';
+import { useAuth } from '../authentication/AuthProvider';
+import { Role } from '../types/interfaces';
+import { isValidEmailFormat } from '../helpers/formatValidation';
 
 export default function Invite(args: any) {
-    const { token } = useAuth();
-    const [email, setEmail] = useState('');
-    const [role, setRole] = useState('');
-    const [organizationId, setOrganizationId] = useState('');
-  
-    const handleSubmit = async (e: any) => {
-      e.preventDefault();
-  
-      sendUserInviteRequest(token!, email, role, Number(organizationId));
-    };
+  const { token } = useAuth();
+  const [email, setEmail] = useState('');
+  const [role, setRole] = useState<Role>(Role.ADMIN);
+  const [organizationId, setOrganizationId] = useState('');
 
-    async function sendUserInviteRequest(token: string, email: string, role: string, organizationId: number) {
-        return fetch(`http://localhost:3000/invitation/user/`, {
-          method: 'POST',
-          headers: {
-            Authorization: token,
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email, role, organizationId }),
-        });
-      }
-    
-        return (
-            <div className="flex flex-col justify-center items-center">
-            <h1>Invite User</h1>
-            <form className="flex flex-col items-center mt-3" onSubmit={handleSubmit}>
-              <label className="mb-4">
-                <p>E-Mail</p>
-                <input className="input input-bordered" type="text" onChange={(e) => setEmail(e.target.value)} />
-              </label>
-              <label className="mb-6">
-                <p>Role</p>
-                <input className="input input-bordered" type="text" onChange={(e) => setRole(e.target.value)} />
-              </label>
-              <label className="mb-6">
-                <p>Organization Id</p>
-                <input className="input input-bordered" type="text" onChange={(e) => setOrganizationId(e.target.value)} />
-              </label>
-              <button className="btn btn-primary mb-6" type="submit">
-                Invite
-              </button>
-            </form>
-          </div>
-        )
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    sendUserInviteRequest(token!, email, role, Number(organizationId));
+  };
+
+  async function sendUserInviteRequest(token: string, email: string, role: Role, organizationId: number) {
+    return fetch(`http://localhost:3000/invitation/user/`, {
+      method: 'POST',
+      headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, role, organizationId }),
+    });
+  }
+
+  function onEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const enteredEmail = event.currentTarget.value;
+
+    if (isValidEmailFormat(enteredEmail)) {
+      setEmail(enteredEmail);
+    } else {
+      throw new Error('Invalid email');
     }
-  
+  }
+
+  function onRoleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    const selectedRole: string = event.currentTarget.value;
+
+    if (selectedRole === Role.ADMIN || selectedRole === Role.CONTRACTOR || selectedRole === Role.FARMER) {
+      setRole(selectedRole);
+    } else {
+      throw new Error('Invalid role');
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h1>Invite User</h1>
+      <form className="flex flex-col items-center mt-3" onSubmit={handleSubmit}>
+        <label className="mb-4">
+          <p>E-Mail</p>
+          <input className="input input-bordered" type="text" onChange={onEmailChange} />
+        </label>
+        <label className="mb-6">
+          <p>Rolle</p>
+          <select className="select select-bordered" onChange={onRoleChange} value={role}>
+            <option selected value={Role.ADMIN}>
+              Admin
+            </option>
+            <option value={Role.CONTRACTOR}>Lohnarbeiter</option>
+            <option value={Role.FARMER}>Landwirt</option>
+          </select>
+        </label>
+        <label className="mb-6">
+          <p>Betrieb</p>
+          <input className="input input-bordered" type="text" onChange={(e) => setOrganizationId(e.target.value)} />
+        </label>
+        <button className="btn btn-primary mb-6" type="submit">
+          Invite
+        </button>
+      </form>
+    </div>
+  );
+}
