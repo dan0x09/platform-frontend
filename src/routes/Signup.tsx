4b80aff0a02999eb0a8445fa93e00c2519267238
dan0x09 <stu126132@mail.uni-kiel.de>
2023-05-22 03:47:21 +0200

    Enhance signup page

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,50 +1,69 @@
-import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { decodeToken } from 'react-jwt';
+import { UserTokenPayload } from '../types/interfaces';
+import { Button } from 'react-daisyui';
 
 export default function Signup(args: any) {
   const [searchParams] = useSearchParams();
   const token = searchParams.get('t');
+  const navigate = useNavigate();
 
+  const [isSendingInviteAcceptRequest, setIsSendingInviteAcceptRequest] = useState(false);
+
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
 
-  const handleSubmit = async (e: any) => {
+  useEffect(() => {
+    if (token) {
+      const decodedToken = decodeToken(token) as ({ email: string } & UserTokenPayload) | null;
+
+      if (!decodedToken) {
+        navigate('/login');
+      } else {
+        setEmail(decodedToken.email);
+      }
+    }
+  }, [token]);
+
+  async function handleSubmit(e: any) {
     e.preventDefault();
 
-    sendInviteAcceptRequest(token!, password, firstName, lastName);
-  };
+    setIsSendingInviteAcceptRequest(true);
+    const response = await sendInviteAcceptRequest(token!, password);
+    setIsSendingInviteAcceptRequest(false);
+
+    if (response.ok) {
+      navigate('/login');
+    }
+  }
 
   return (
     <div className="login-wrapper h-screen flex flex-col justify-center items-center bg-gray-50">
       <h1>Einladung</h1>
       <form className="flex flex-col items-center mt-3" onSubmit={handleSubmit}>
-        <label className="mb-6">
+        <label className="mb-3">
+          <p>E-Mail-Adresse</p>
+          <input className="input input-bordered" type="text" readOnly disabled value={email} />
+        </label>
+        <label className="mb-5">
           <p>Passwort</p>
           <input className="input input-bordered" type="password" onChange={(e) => setPassword(e.target.value)} />
         </label>
-        <label className="mb-6">
-          <p>Vorname</p>
-          <input className="input input-bordered" type="text" onChange={(e) => setFirstName(e.target.value)} />
-        </label>
-        <label className="mb-6">
-          <p>Nachname</p>
-          <input className="input input-bordered" type="text" onChange={(e) => setLastName(e.target.value)} />
-        </label>
-        <button className="btn btn-primary mb-6" type="submit">
+        <Button color="primary" className={`mb-6 ${isSendingInviteAcceptRequest ? 'loading' : ''}`} type="submit">
           Beitreten
-        </button>
+        </Button>
       </form>
     </div>
   );
 }
 
-async function sendInviteAcceptRequest(token: string, password: string, firstName: string, lastName: string) {
+async function sendInviteAcceptRequest(token: string, password: string) {
   return fetch(`${process.env.REACT_APP_BACKEND_URL}/invitation/user/accept`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ token, password, firstName, lastName }),
+    body: JSON.stringify({ token, password }),
   });
 }
